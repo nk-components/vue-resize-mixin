0ac38ccc05fcbf8d684e50fc76c9f5ab5c2bb8cd
Florian Morel <ayamflow@gmail.com>
2014-12-04 13:56:05 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var windowsill = require('windowsill');
+var resizer = windowsill.resizer;
+
+module.exports = {
+  ready: function() {
+    windowsill.enable('resizer');
+    resizer.addListener(this._resize);
+    this._resize();
+  },
+
+  beforeDestroy: function() {
+    resizer.removeListener(this._resize);
+  },
+
+  methods: {
+      _resize: function() {
+        this.$emit('resize', {width: resizer.width, height: resizer.height});
+      }
+  }
+};
